596991830b46bc18dc341b8f1dc587f0ebcbf6e4
Sam Palmer <spalmer@stu.jsu.edu>
2019-09-01 15:17:53 -0500

    Added f3()

--- Class2.java
+++ Class2.java
@@ -4,5 +4,8 @@ public class Class2 {
     } 
     private void f2() {      
         // Placeholder method (v2.0)   
+    }
+    private void f3() {      
+        // Placeholder method (v3.0)   
     } 
 }  
--- HelloWorld.java
+++ HelloWorld.java
@@ -9,5 +9,8 @@ public class HelloWorld {
     } 
     private void f2() {      
         // Placeholder method (v2.0)   
-    } 
+    }
+    private void f3() {      
+        // Placeholder method (v3.0)   
+    }     
 } 
